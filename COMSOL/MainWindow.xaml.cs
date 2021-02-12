745f89edc8ba15041f36a716a1ed557cdc38033e
Mahdi Maharebi <mmaharebi@hotmail.com>
2021-02-12 19:16:07 +0330

    menu control code added

--- COMSOL/MainWindow.xaml.cs
+++ COMSOL/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace COMSOL
         {
             InitializeComponent();
         }
+
+        private void exit_menuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mainWindow_window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Do you want to exit?", "Exit Application", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void about_menuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            string about = "About COMSOL";
+            MessageBox.Show(about, "About", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
